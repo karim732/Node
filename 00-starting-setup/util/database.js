235b2efc886bfb327dbf69ab4fed0a3f3c2a6876
karim732 <karimshaik5252@gmail.com>
2024-01-05 20:48:40 +0530

    Initial Commit

---
+++ 00-starting-setup/util/database.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require("mongodb");
+
+let _db;
+
+const mongoConnect = (callback) => {
+  // GjmyHrbbSEXJLiDX
+  const uri =
+    "mongodb+srv://karimshaik5252:GjmyHrbbSEXJLiDX@cluster0.fsc0kdu.mongodb.net/Node";
+  MongoClient.connect(uri)
+    .then((dbclient) => {
+      console.log("connected");
+      _db = dbclient.db("shop");
+      callback();
+    })
+    .catch((err) => {
+      console.log("failed");
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Node course
+
+MongoDB - NoSql, Schemaless
+
+MongoDB is used for humongous data.
+
+Database -> Collections(tables) -> Documents.
+
+Making less or no relation is important, as relations slows down quering.
+
+1. Connect MongoDb to application.
