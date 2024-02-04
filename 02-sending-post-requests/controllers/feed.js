3020caff2bc8c7ea4675a4028e1d2176460e0044
karim732 <karimshaik5252@gmail.com>
2024-02-04 18:19:18 +0530

    classic to restapi, Authentication and authorization.

--- 02-sending-post-requests/controllers/feed.js
+++ 02-sending-post-requests/controllers/feed.js
@@ -1,43 +1,167 @@
-const { validationResults } = require("express-validator");
+const fs = require("fs")
+const path = require('path')
+const { validationResult } = require("express-validator");
+const Post = require("../models/posts")
+const User = require('../models/user')
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "images/Data Science.jpg",
-        creator: {
-          name: "karimulla",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
+  Post.find().countDocuments().then(count => {
+    totalItems = count;
+    return Post.find().skip((currentPage - 1) * perPage).limit(perPage).populate("creator")
+  }).then(posts => {
+    res.status(200).json({ message: 'Fetched posts successfully', posts: posts, totalItems: totalItems })
+  })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err)
+    })
 };
 
 exports.createPost = (req, res, next) => {
-  const errors = validationResults(req);
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: "Validation failed, entered data is incorrect.",
-      errors: errors.array(),
-    });
+    const error = new Error("Validation failed, entered data is incorrect.")
+    error.statusCode = 422;
+    throw error
+  }
+  // console.log(req);
+  if (!req.file) {
+    const error = new Error("No image provided.")
+    error.statusCode = 422
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {
-        name: "Karimulla",
-      },
-      createdAt: new Date(),
-    },
-  });
+  let creator;
+  const imageUrl = req.file.path.replace("\\", "/")
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: imageUrl,
+    creator: req.userId
+  })
+  post.save().then(result => {
+    return User.findById(req.userId);
+  }).then( user =>{
+    creator = user
+    user.posts.push(post)
+    return user.save()
+  }).then(result =>{
+    res.status(201).json({
+      message: "Post created successfully!",
+      post: post,
+      creator:{_id: creator._id, name:creator.name}
+    });
+  }).catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err)
+  })
 };
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId).then(post => {
+    if (!post) {
+      const error = new Error('Could not find post.')
+      error.statusCode = 400;
+      throw error
+    }
+    res.status(200).json({ message: 'Post fetched.', post: post })
+  }).catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err)
+  })
+}
+
+exports.updatePost = (req, res, next) => {
+
+  const postId = req.params.postId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect.")
+    error.statusCode = 422;
+    throw error
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path.replace("\\", "/")
+  }
+  if (!imageUrl) {
+    const error = new Error('No file picked.')
+    error.statusCode = 422;
+    throw error
+  }
+  Post.findById(postId).then(post => {
+    if (!post) {
+      const error = new Error('Could not find post.')
+      error.statusCode = 400;
+      throw error
+    }
+    if(post.creator.toString() !== req.userId ){
+      const error = new Error('Not authorized!');
+      error.statusCode = 403;
+      throw error;
+    }
+    if (imageUrl !== post.imageUrl) {
+      clearImage(post.imageUrl)
+    }
+    post.title = title;
+    post.imageUrl = imageUrl
+    post.content = content
+    return post.save();
+  }).then(result => {
+    res.status(201).json({ message: 'Post updated.', post: result })
+  }).catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err)
+  })
+}
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId).then(post => {
+    if (!post) {
+      const error = new Error('Could not find post.')
+      error.statusCode = 400;
+      throw error
+    }
+    //check logged in user
+    if(post.creator.toString() !== req.userId ){
+      const error = new Error('Not authorized!');
+      error.statusCode = 403;
+      throw error;
+    }
+    clearImage(post.imageUrl)
+    return Post.findByIdAndDelete(postId)
+  }).then(result => {
+    return User.findById(req.userId)
+  }).then(user =>{
+    user.posts.pull(postId)
+    return user.save()
+  }).then(result =>{
+    res.status(200).json({ message: 'Deleted post.' })
+  }).catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err)
+  })
+}
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, "..", filePath)
+  fs.unlink(filePath, err => console.log(err))
+}
--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 Node course
 
+server side rendering -> SPA(using RestApi)
+
 Frontend and backend code on different servers.
+
+In async then, catch throw error doesn't work, next(error) works.
+
+Rest api is stateless and sessions is not used.
+
+In rest api, we return so called token to the client, which is generated on the server and this token is stored on the client.
+
+Client can then attach this token to every subsequent request it sends to the server. 
+
+JSON Data + Signature(can be verified by server via secret key) = JSON Web Token(JWT)
+
+Authorization
+
+
+**
+one good way to mitigate against XSS attacks when storing the JWT in localStorage is to split the token up into two pieces. One containing the headers and payload, which you store in localStorage, and then send the signature separately in HTTP only Cookie. You will of course need to add logic to join these two pieces together in both th BE and FE. This means that if somebody was able to steal the part of the token in the localStorage, they would not have access to the signature from JS. 
